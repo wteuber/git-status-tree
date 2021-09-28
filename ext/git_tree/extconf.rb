d2a975cd55ab0d153f55621fd04862cc2225d5dd
Wolfgang Teuber <knugie@gmx.net>
2021-09-28 11:21:04 +0200

    Run git_add_alias_tree after gem install git-status-tree
    Post-install hooks defined via Gem.post_install didn't work reliably.
    This workaround (hack) is used to execute git_add_alias_tree after the
    gem was installed.

---
+++ ext/git_tree/extconf.rb
@@ -0,0 +1,15 @@
+require 'mkmf'
+
+find_executable('bash')
+find_executable('git')
+find_executable('make')
+
+# Trick Rubygems into thinking the generated Makefile was executed
+compile = File.join(Dir.pwd, 'git_tree.' + RbConfig::CONFIG['DLEXT'])
+File.open(compile, "w") {}
+
+# Install "git tree"
+puts `../../bin/git_add_alias_tree`
+
+# Trick Rubygems into thinking the Makefile was executed
+$makefile_created = true
