46d8fda7f585a03069046b27f1dab4764ab0186b
Wolfgang Teuber <knugie@gmx.net>
2023-02-06 02:52:55 +0100

    Drop support for Ruby < 2.7

--- ext/git_tree/extconf.rb
+++ ext/git_tree/extconf.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'mkmf'
 
 find_executable('bash')
@@ -5,11 +7,11 @@ find_executable('git')
 find_executable('make')
 
 # Trick Rubygems into thinking the generated Makefile was executed
-compile = File.join(Dir.pwd, 'git_tree.' + RbConfig::CONFIG['DLEXT'])
-File.open(compile, "w") {}
+compile = File.join(Dir.pwd, "git_tree.#{RbConfig::CONFIG['DLEXT']}")
+File.open(compile, 'w') {}
 
 # Install "git tree"
 puts `../../bin/git_add_alias_tree`
 
 # Trick Rubygems into thinking the Makefile was executed
-$makefile_created = true
+$makefile_created = true # rubocop:disable Style/GlobalVars
--- lib/node.rb
+++ lib/node.rb
@@ -1,11 +1,11 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 class NodeNameError < StandardError; end
 class NodeChildrenError < StandardError; end
 class NodeTypeError < StandardError; end
 
 # A Node represents a file or directory in the git-status-tree
-class Node
+class Node # rubocop:disable Metrics/ClassLength
   class << self
     attr_accessor :indent
   end
@@ -14,12 +14,8 @@ class Node
 
   def initialize(name, children = nil, status = nil)
     self.class.indent ||= 4
-    msg = '"name" must be a String.'
-    raise NodeNameError, msg unless name.is_a? String
-    msg = '"name" must have at least one character.'
-    raise NodeNameError, msg if name.empty?
-    msg = '"name" must not contain "/", use create_from_string.'
-    raise NodeNameError, msg if name =~ /\//
+    validate_name!(name)
+
     msg = '"children" must be a NodesCollection or nil.'
     valid_nodes_collection = children.nil? || children.is_a?(NodesCollection)
     raise NodeChildrenError, msg unless valid_nodes_collection
@@ -33,32 +29,39 @@ class Node
     msg = '"str_node" must be String.'
     raise NodeTypeError, msg unless gs_porcelain.is_a? String
     raise NodeNameError, '"str_node" too short.' if gs_porcelain.length < 4
-    status = if gs_porcelain[1..1] == ' '
-               gs_porcelain[0..0] + '+'
-             else
-               gs_porcelain[1..1]
-             end
-    path = './' + gs_porcelain[3..-1]
-    ary_nodes = path.split(/\//)
-    name = ary_nodes.shift
 
-    if ary_nodes.any?
-      children = NodesCollection.create_from_array(ary_nodes, status)
-      node = self.new(name, children)
-    else
-      node = self.new(name, nil, status)
-    end
-
-    node
+    node_from_gs(gs_porcelain)
   end
 
   def self.instances?
-    lambda { |node| node.is_a?(Node) }
+    ->(node) { node.is_a?(Node) }
   end
+
+  def self.node_from_gs(gs_porcelain)
+    status = status(gs_porcelain)
+    ary_nodes = "./#{gs_porcelain[3..]}".split(%r{/})
+
+    name = ary_nodes.shift
+    if ary_nodes.any?
+      children = NodesCollection.create_from_array(ary_nodes, status)
+      new(name, children)
+    else
+      new(name, nil, status)
+    end
+  end
+
+  private_class_method :node_from_gs
+
+  def self.status(gs_porcelain)
+    status = "#{gs_porcelain[0]}+" if gs_porcelain[1] == ' '
+    status = gs_porcelain[1] unless gs_porcelain[1] == ' '
+    status
+  end
+  private_class_method :status
 
   def to_primitive
     if dir?
-      {name => children.to_primitive}
+      { name => children.to_primitive }
     else
       name
     end
@@ -75,98 +78,109 @@ class Node
   def valid?
     return valid_dir? if dir?
     return valid_file? if file?
+
     false
   end
 
   # @return [NodesCollection]
   def +(other)
-    raise 'not valid' unless self.valid? && other.valid?
-    raise 'not a ' + self.class.to_s unless other.is_a?(self.class)
+    raise 'not valid' unless valid? && other.valid?
+    raise "not a #{self.class}" unless other.is_a?(self.class)
 
-    tmp_children = [self.children, other.children].compact.inject(&:+)
+    tmp_children = [children, other.children].compact.inject(&:+)
 
-    NodesCollection.new([self.class.new(self.name, tmp_children)])
+    NodesCollection.new([self.class.new(name, tmp_children)])
   end
 
   def <=>(other)
-    return (self.name <=> other.name) if self.file? == other.file?
-    return -1 if (self.dir? && other.file?)
-    return 1 if (self.file? && other.dir?)
+    return (name <=> other.name) if file? == other.file?
+    return -1 if dir? && other.file?
+    return 1 if file? && other.dir?
+
     0
   end
 
-  def to_tree_s(depth = 0, open_parents = [0], last = true)
+  def to_tree_s(depth = 0, open_parents = [0], last: true)
     open_parents << depth
 
     pre = pre_tree(depth, open_parents, last)
 
-    color_name = ''
-    if dir?
-      color_name += BashColor::EMB + name
-    else #file?
-      if staged?
-        color_name += BashColor::G
-      else
-        color_name += BashColor::R
-      end
-      color_name += name + ' (' + status + ')'
-    end
-    color_name +=  BashColor::NONE
-
-
-    str_tree = pre + color_name + "\n"
-    str_tree << children.to_tree_s(depth + 1, open_parents) if children
+    str_tree = "#{pre}#{color_name}\n"
+    str_tree += children.to_tree_s(depth + 1, open_parents) if children
 
     str_tree
   end
 
   #   'M'  modified
   def modified?
-    self.status.include?('M')
+    status.include?('M')
   end
 
   #   'A'  added
   def added?
-    self.status.include?('A')
+    status.include?('A')
   end
 
   #   'D'  deleted
   def deleted?
-    self.status.include?('D')
+    status.include?('D')
   end
 
   #   'R'  renamed
   def renamed?
-    self.status.include?('R')
+    status.include?('R')
   end
 
   #   'C'  copied
   def copied?
-    self.status.include?('C')
+    status.include?('C')
   end
 
   #   'U'  updated but unmerged
   def unmerged?
-    self.status.include?('U')
+    status.include?('U')
   end
 
   #   '?' new
   def new?
-    self.status.include?('?')
+    status.include?('?')
   end
 
   #   '+' staged
   def staged?
-    self.status.include?('+')
+    status.include?('+')
   end
 
   private
+
+  def validate_name!(name)
+    msg = '"name" must be a String.'
+    raise NodeNameError, msg unless name.is_a? String
+
+    msg = '"name" must have at least one character.'
+    raise NodeNameError, msg if name.empty?
+
+    msg = '"name" must not contain "/", use create_from_string.'
+    raise NodeNameError, msg if name =~ %r{/}
+  end
+
+  def color_name
+    color_name = ''
+    if dir?
+      color_name += BashColor::EMB + name
+    else
+      color_name += BashColor::G if staged?
+      color_name += BashColor::R unless staged?
+      color_name += "#{name} (#{status})"
+    end
+    color_name + BashColor::NONE
+  end
+
   # Has a valid name?
   def name_valid?
-    name &&
-      name.is_a?(String) &&
-      (name.length > 0) &&
-      name.match(/\//).nil?
+    name.is_a?(String) &&
+      name.length.positive? &&
+      name.match(%r{/}).nil?
   end
 
   # Is a valid dir?
@@ -183,26 +197,27 @@ class Node
   end
 
   def pre_tree(depth, open_parents, last)
-    if depth == 0
+    if depth.zero?
       ''
-    elsif depth > 0
+    elsif depth.positive?
       pre_ary = Array.new(depth).fill('    ')
-
       indent = self.class.indent - 2
-      open_parents.each do |idx|
-        pre_ary[idx] = '│' + (' ' * indent) + ' ' if pre_ary[idx] == '    '
-      end
 
-      if last
-        pre_ary[-1] = '└'
-        open_parents.delete(depth-1)
-      else
-        pre_ary[-1] = '├'
-      end
-      pre_ary[-1] += ('─' * indent) + ' '
+      open_parents.each { |idx| pre_ary[idx] = "│#{' ' * indent} " if pre_ary[idx] == '    ' }
 
+      sibling(depth, indent, last, open_parents, pre_ary)
 
       pre_ary * ''
     end
+  end
+
+  def sibling(depth, indent, last, open_parents, pre_ary)
+    if last
+      pre_ary[-1] = '└'
+      open_parents.delete(depth - 1)
+    else
+      pre_ary[-1] = '├'
+    end
+    pre_ary[-1] += "#{'─' * indent} "
   end
 end
